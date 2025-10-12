cfca6185b8714aa917fa096abba9db6a4b40d33d
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2025-10-13 00:59:43 +0600

    other modules deleted

--- src/app/modules/tourType/tourType.validation.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const createTourTypeZodSchema = z.object({
-  name: z.string({ invalid_type_error: "Tour type must have string" }),
-});
-
-export const updateTourTypeZodSchema = z.object({
-  name: z.string({ invalid_type_error: "Tour type must have string" }),
-});
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,15 +1,10 @@
 import { Router } from 'express';
 import { AuthRoutes } from '../modules/auth/auth.route';
 import { BlogRoutes } from '../modules/blog/blog.route';
-import { BookingRoutes } from '../modules/booking/booking.route';
 import { CommentRoute } from '../modules/comment/comment.route';
-import { DivisionRoutes } from '../modules/division/division.route';
 import { OtpRoutes } from '../modules/otp/otp.route';
-import { PaymentRoutes } from '../modules/payment/payment.route';
 import { ReactionRoute } from '../modules/reaction/reaction.route';
 import { StatsRoutes } from '../modules/stats/stats.route';
-import { TourRoutes } from '../modules/tour/tour.route';
-import { TourTypeRoutes } from '../modules/tourType/tourType.route';
 import { UserRoutes } from '../modules/user/user.route';
 
 export const router = Router();
@@ -23,26 +18,7 @@ const moduleRoutes = [
     path: '/auth',
     route: AuthRoutes,
   },
-  {
-    path: '/division',
-    route: DivisionRoutes,
-  },
-  {
-    path: '/tour',
-    route: TourTypeRoutes,
-  },
-  {
-    path: '/tour',
-    route: TourRoutes,
-  },
-  {
-    path: '/booking',
-    route: BookingRoutes,
-  },
-  {
-    path: '/payment',
-    route: PaymentRoutes,
-  },
+
   {
     path: '/otp',
     route: OtpRoutes,
