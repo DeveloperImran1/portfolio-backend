19bda196f584e5858a390bdd4c39dec17d15b552
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2025-10-16 14:45:48 +0600

    re build in localy

--- dist/app/modules/tour/tour.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// tour ke search korar jonno kon kono field a search korbo, seita akta array er moddhe rakte pari. Aita jeheto akta hardcoded vabe thakbe, and pore change hote pare. Tai tour.constant.ts file a extra vabe rakha hoisa.
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tourSearchableFields = void 0;
-exports.tourSearchableFields = ["title", "description", "location"];
