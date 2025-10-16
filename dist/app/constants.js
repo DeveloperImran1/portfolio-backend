b16af200ab7324b8d1944d771d5caa434550dc8b
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2025-10-16 15:03:52 +0600

    dist folder remove from github

--- dist/app/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.excludedField = void 0;
-exports.excludedField = ["searchTerm", "sort", "fields", "page", "limit"];
